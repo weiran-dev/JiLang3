c35d6f9354c1755da398d1854f350ffbb11a2ff1
witheredAd <tiflew123@gmail.com>
2020-02-06 15:17:51 +0800

    new commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+auto welcomeText = "及语言 V3.0 Alpha4 Aureole\n\
+采用 LGPLv3 开源许可协议。\n\
+";
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << welcomeText << endl;
+
     return 0;
 }
